a47d99ac64d2f18126ca21f01dbbc04a2a1da7fd
kobayashi-yabako <52003784+kobayashiyabako16g@users.noreply.github.com>
2026-01-24 06:19:48 +0000

    Update dpp-cli to 1.0.3

--- Formula/dpp-cli.rb
+++ Formula/dpp-cli.rb
@@ -1,22 +1,22 @@
 class DppCli < Formula
   desc "CLI tool for managing dpp.vim plugins."
   homepage "https://github.com/kobayashiyabako16g/dpp-cli"
-  version "1.0.2"
+  version "1.0.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kobayashiyabako16g/dpp-cli/releases/download/v1.0.2/dpp-cli-macos-aarch64.tar.gz"
-      sha256 "20ec65aaa07997d779ceea706d12965c6aeeda25ee125c56d2ab96567893d23c"
+      url "https://github.com/kobayashiyabako16g/dpp-cli/releases/download/v1.0.3/dpp-cli-macos-aarch64.tar.gz"
+      sha256 "61cb7527a3e9489e4289ff1cb0f1e985c6655c5cb6f0d2be383f33e6db536acb"
     else
-      url "https://github.com/kobayashiyabako16g/dpp-cli/releases/download/v1.0.2/dpp-cli-macos-x86_64.tar.gz"
-      sha256 "d334e0de7461915974f6b1c5379cf2c6dc4e3c7e27ba1b7ee47077a92c51a8a8"
+      url "https://github.com/kobayashiyabako16g/dpp-cli/releases/download/v1.0.3/dpp-cli-macos-x86_64.tar.gz"
+      sha256 "7642a31de29cfe5439245f29a905c22930c9b12f0b25006f2eb4948e7652fc27"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/kobayashiyabako16g/dpp-cli/releases/download/v1.0.2/dpp-cli-linux-x86_64.tar.gz"
+      url "https://github.com/kobayashiyabako16g/dpp-cli/releases/download/v1.0.3/dpp-cli-linux-x86_64.tar.gz"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
